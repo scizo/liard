6dc77b6e9c4b73e3b5216c05644f5818d0f7d804
Scott Nielsen <scottnielsen5@gmail.com>
2011-10-26 14:27:02 -0600

    Removing unused client.

--- lib/liard.rb
+++ lib/liard.rb
@@ -1,9 +1,1 @@
-require 'eventmachine'
 require 'liard/server'
-require 'liard/client'
-
-module Liard
-  def self.start_client(host, port, output=STDOUT)
-    EM.connect(host, port, Client, output)
-  end
-end
