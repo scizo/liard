8226f80dfe57c85bf92feafbc8b32f31a47ed0a6
Scott Nielsen <scottnielsen5@gmail.com>
2011-12-19 17:23:20 -0700

    Moving TestClient to env.rb

--- features/step_definitions/client_steps.rb
+++ features/step_definitions/client_steps.rb
@@ -1,34 +1,3 @@
-class TestClient
-  attr_reader :buffer
-
-  def initialize(host, port)
-    @socket = Socket.tcp(host, port)
-    @buffer = BufferedTokenizer.new "\r\n"
-    @messages = []
-  end
-
-  def connected?
-    !select([@socket], nil, nil, 0.1)
-  end
-
-  def send_command(data)
-    @socket.write("#{data}\r\n")
-  end
-
-  def messages
-    return @messages.concat(@buffer.extract(text)) if @messages.empty?
-    @messages
-  end
-
-  def text
-    @text ||= @socket.recv_nonblock(100000)
-  end
-
-  def close
-    @socket.close
-  end
-end
-
 Before do
   Liard::Random.seed(5)
   thread = Thread.new { EM.run }
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,4 +1,34 @@
 require 'simplecov' if ENV['COVERAGE']
 $LOAD_PATH << File.expand_path('../../../lib', __FILE__)
-require 'evented-spec'
 require 'liard'
+
+class TestClient
+  attr_reader :buffer
+
+  def initialize(host, port)
+    @socket = Socket.tcp(host, port)
+    @buffer = BufferedTokenizer.new "\r\n"
+    @messages = []
+  end
+
+  def connected?
+    !select([@socket], nil, nil, 0.1)
+  end
+
+  def send_command(data)
+    @socket.write("#{data}\r\n")
+  end
+
+  def messages
+    return @messages.concat(@buffer.extract(text)) if @messages.empty?
+    @messages
+  end
+
+  def text
+    @text ||= @socket.recv_nonblock(100000)
+  end
+
+  def close
+    @socket.close
+  end
+end
